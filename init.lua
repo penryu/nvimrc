d7a52b51a397b1644a86d9ea0c6d1048cba351b5
Tim Hammerquist <tim@penryu.dev>
2023-01-26 04:59:03 -0800

    add neovide settings

--- init.lua
+++ init.lua
@@ -79,3 +79,22 @@ vim.cmd [[
    "autocmd ColorScheme * highlight! link CocRustTypeHint CocListYellowCyan
    "autocmd ColorScheme * highlight! link CocRustChainingHint CocListYellowBlue
 ]]
+
+if vim.g.neovide == true then
+   vim.opt.guifont = { 'SauceCodePro Nerd Font Mono:h14' }
+   -- vim.g.neovide_transparency = 1.0
+   -- vim.g.transparency = 1.0
+   -- vim.g.neovide_floating_blur_amount_x = 2.0
+   -- vim.g.neovide_floating_blur_amount_y = 2.0
+   vim.g.neovide_scroll_animation_length = 0.9
+   -- vim.g.neovide_hide_mouse_when_typing = false
+   -- vim.g.neovide_refresh_rate = 60
+   vim.g.neovide_refresh_rate_idle = 5
+   -- vim.g.neovide_confirm_quit = true
+   -- vim.g.neovide_profiler = true
+   -- vim.g.neovide_fullscreen = false
+   vim.g.neovide_cursor_animation_length = 0.05
+   -- vim.g.neovide_cursor_antialiasing = true
+   -- vim.g.neovide_cursor_trail_size = 0.8
+   vim.g.neovide_cursor_vfx_mode = ''
+end
