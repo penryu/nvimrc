b497d98b9230c177d30558017cfd9b98424bad5c
Tim Hammerquist <tim@penryu.dev>
2023-04-01 02:31:25 -0700

    tweak runtime loading

--- init.lua
+++ init.lua
@@ -7,38 +7,12 @@ local buf_set_option = vim.api.nvim_buf_set_option
 
 local g, o = vim.g, vim.o
 
--- prevent builtin plugins from loading
-if false then
-   -- g.loaded_gzip = 1
-   -- g.loaded_zip = 1
-   -- g.loaded_zipPlugin = 1
-   -- g.loaded_tar = 1
-   -- g.loaded_tarPlugin = 1
-
-   -- g.loaded_getscript = 1
-   -- g.loaded_getscriptPlugin = 1
-   -- g.loaded_vimball = 1
-   -- g.loaded_vimballPlugin = 1
-   -- g.loaded_2html_plugin = 1
-
-   -- g.loaded_matchit = 1
-   -- g.loaded_matchparen = 1
-   -- g.loaded_logiPat = 1
-   -- g.loaded_rrhelper = 1
-
-   -- netrw init
-   g.loaded_netrw = 1
-   g.loaded_netrwPlugin = 1
-   g.loaded_netrwSettings = 1
-   g.loaded_netrwFileHandlers = 1
-
-   -- runtime providers
-   -- g.loaded_node_provider = 1
-   g.loaded_python_provider = 1
-   -- g.loaded_python3_provider = 1
-   g.loaded_ruby_provider = 1
-   g.loaded_perl_provider = 1
-end
+-- prevent runtime providers from loading
+g.loaded_perl_provider = 0
+g.loaded_python_provider = 0
+-- g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+-- g.loaded_node_provider = 0
 
 require 'keys'
 
