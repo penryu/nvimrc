c1b8afa84515b12ae60513053ce500731c9397cc
Tim Hammerquist <tim@penryu.dev>
2023-12-11 05:57:00 -0800

    feat(git): add neogit, a very promising magit port

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -31,4 +31,14 @@ return {
          'GBrowse',
       },
    },
+   {
+      'NeogitOrg/neogit',
+      dependencies = {
+         'nvim-lua/plenary.nvim', -- required
+         'nvim-telescope/telescope.nvim', -- optional
+         'sindrets/diffview.nvim', -- optional
+         'ibhagwan/fzf-lua', -- optional
+      },
+      config = true,
+   },
 }
