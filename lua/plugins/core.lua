b2c138ffcfb501c068f5052bdfa35b270a0f1e17
Tim Hammerquist <tim@penryu.dev>
2024-01-20 22:16:48 -0800

    feat(undotree)

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -549,6 +549,14 @@ return {
     },
   },
   {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
+  {
     'tpope/vim-commentary',
     keys = {
       { 'gc', mode = { 'n', 'o', 'x' } },
