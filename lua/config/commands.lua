c65a486c527ff5862d902060451654dc216e9b9a
Tim Hammerquist <tim@penryu.dev>
2024-02-13 04:13:55 -0800

    chore: clean up unused commands

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -4,8 +4,5 @@
 
 local u = require('util')
 
--- :Sh opens term in new window
-u.create_command('Sh', 'new +terminal', { nargs = '*' })
-
 -- Remove trailing carriage returns from misdetected DOS files
 u.create_command('DeWinify', '%s/\r$//', { nargs = 0 })
