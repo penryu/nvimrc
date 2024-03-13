0d7ee29589d946d26f337d36e9970fb9d5eb4237
Tim Hammerquist <tim@penryu.dev>
2024-03-13 14:07:43 -0700

    feat(conform.nvim): enable opting out of auto-format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -244,10 +244,14 @@ return {
     'stevearc/conform.nvim',
     config = function()
       require('conform').setup {
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
-        },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_fallback = true }
+        end,
+
         formatters = {
           comrak = {
             command = 'comrak',
@@ -268,9 +272,26 @@ return {
         end,
         { nargs = 0 }
       )
+      u.create_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+      u.create_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
     end,
     event = 'BufWritePre',
-    cmd = { 'ConformInfo', 'Format' },
+    cmd = { 'ConformInfo', 'Format', 'FormatDisable', 'FormatEnable' },
     keys = {
       {
         '<leader>f',
